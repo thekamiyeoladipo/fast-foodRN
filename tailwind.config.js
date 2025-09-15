13a1eb0a45d449551a917e5b41135ce092715637
kamiye <kamiye31@gmail.com>
2025-09-15 18:38:27 +0100

    fix: remove outdated comment and restore font family configuration in Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
@@ -23,9 +22,13 @@ module.exports = {
         success: "#2F9B65",
       },
       fontFamily: {
-        
-      }
+        quicksand: ["Quicksand-Regular", "sans-serif"],
+        "quicksand-bold": ["Quicksand-Bold", "sans-serif"],
+        "quicksand-semibold": ["Quicksand-SemiBold", "sans-serif"],
+        "quicksand-light": ["Quicksand-Light", "sans-serif"],
+        "quicksand-medium": ["Quicksand-Medium", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
+};
