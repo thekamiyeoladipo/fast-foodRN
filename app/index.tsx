9929874271b9ec42478d755f45fd9d48a26f3ba7
kamiye <kamiye31@gmail.com>
2025-09-15 20:43:17 +0100

    feat: added images to the index page

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,37 @@
+import { offers } from "@/constants";
 import "./global.css"
-import { FlatList, Text, View } from "react-native";
+import { FlatList, Image, Pressable, Text, TouchableOpacity, View} from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
- 
+import { Fragment } from "react";
+
 export default function Index() {
   return (
     <SafeAreaView>
-      <FlatList data={} 
-      renderItem={({ item }) => ()} 
+      <FlatList data={offers} 
+      renderItem={({ item, index }) => {
+        return (
+          <View>
+            <Pressable
+              className="offer-card"
+              style={{ backgroundColor: item.color }}
+            >
+              {({ pressed }) => (
+                <Fragment>
+                  <View className={"h-full w-1/2"}>
+                    <Image
+                      source={item.image}
+                      className={"size-full"}
+                      resizeMode={"contain"}
+                    />
+                  </View>
+
+                
+                </Fragment>
+              )}
+            </Pressable>
+          </View>
+        );
+      }} 
       />
 
     </SafeAreaView>
