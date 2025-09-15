fa490eadebf2c131dc8f796226c3a8f2028b4a45
kamiye <kamiye31@gmail.com>
2025-09-15 19:47:47 +0100

    feat: update layout and styles for improved font loading and UI consistency

--- app/index.tsx
+++ app/index.tsx
@@ -3,9 +3,9 @@ import { Text, View } from "react-native";
  
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+    <View className="flex-1 items-center justify-center bg-dark-100">
+      <Text className="font-quicksand-bold text-5xl font-bold text-blue-500">
+        Fast Food!
       </Text>
     </View>
   );
