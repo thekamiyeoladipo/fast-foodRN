3672f532e6a84db32d8858304ccb45b14d20418b
kamiye <kamiye31@gmail.com>
2025-09-16 11:59:49 +0100

    feat: enhance offer card layout and add CartButton component

--- app/index.tsx
+++ app/index.tsx
@@ -1,47 +1,86 @@
-import { offers } from "@/constants";
-import "./global.css"
-import { FlatList, Image, Pressable, Text, TouchableOpacity, View} from "react-native";
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { images, offers } from "@/constants";
+import "./global.css";
+import {
+  FlatList,
+  Image,
+  Pressable,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { Fragment } from "react";
-import cn from 'clsx';
+import cn from "clsx";
 
 export default function Index() {
   return (
-    <SafeAreaView>
-      <FlatList data={offers} 
-      renderItem={({ item, index }) => {
-        const isEven = index % 2 === 0;
-        return (
-          <View>
-            <Pressable
-               className={cn("offer-card", isEven ? 'flex-row-reverse' : 'flex-row')}
-              style={{ backgroundColor: item.color }}
-            >
-              {({ pressed }) => (
-                <Fragment>
-                  <View className={"h-full w-1/2"}>
+    <SafeAreaView className="flex-1 bg-white">
+      <FlatList
+        data={offers}
+        renderItem={({ item, index }) => {
+          const isEven = index % 2 === 0;
+          return (
+            <View>
+              <Pressable
+                className={cn(
+                  "offer-card",
+                  isEven ? "flex-row-reverse" : "flex-row"
+                )}
+                style={{ backgroundColor: item.color }}
+                android_ripple={{ color: "#fff22" }}
+              >
+                {({ pressed }) => (
+                  <Fragment>
+                    <View className={"h-full w-1/2"}>
+                      <Image
+                        source={item.image}
+                        className={"size-full"}
+                        resizeMode={"contain"}
+                      />
+                    </View>
+
+                    <View
+                      className={cn(
+                        "offer-card__info",
+                        isEven ? "pl-10" : "pr-10"
+                      )}
+                    >
+                      <Text className="h1-bold text-white leading-tight">
+                        {item.title}
+                      </Text>
+                    </View>
+
                     <Image
-                      source={item.image}
-                      className={"size-full"}
-                      resizeMode={"contain"}
+                      source={images.arrowRight}
+                      className="size-10"
+                      resizeMode="contain"
+                      tintColor="#ffffff"
                     />
-                  </View>
-
-                  <View className={"offer-card__info"}>
-                    <Text>
-                      {item.title}
-                    </Text>
-                  </View>
-
-                
-                </Fragment>
-              )}
-            </Pressable>
+                  </Fragment>
+                )}
+              </Pressable>
+            </View>
+          );
+        }}
+        contentContainerClassName="pb-28 px-5"
+        ListHeaderComponent={() => (
+          <View className="flex-between flex-row w-full my-5 px-5">
+            <View className="flex-start">
+              <Text className="small-bold text-primary">Deliver To</Text>
+              <TouchableOpacity className="flex-row flex-center gap-x-1 mt-0.5">
+                <Text className="paragraph-bold">Nigeria</Text>
+                <Image
+                  className="size-3"
+                  resizeMode="contain"
+                  source={images.arrowDown}
+                />
+              </TouchableOpacity>
+            </View>
+            <Text>Cart</Text>
           </View>
-        );
-      }} 
+        )}
       />
-
     </SafeAreaView>
   );
 }
