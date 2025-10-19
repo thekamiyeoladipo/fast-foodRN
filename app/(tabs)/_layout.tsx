7a7caa1b979c66e7113222556866257cfbe467c4
kamiye <kamiye31@gmail.com>
2025-10-19 21:31:57 +0100

    feat: add SignIn button and redirect logic for authentication in layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,9 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Slot } from 'expo-router'
+import { Slot, Redirect } from 'expo-router'
 
 const _layout = () => {
+  const isAuthenticated = false // Replace with your authentication logic
+
+  if (!isAuthenticated) return <Redirect href="/sign-in" />
   return <Slot />
 }
 
