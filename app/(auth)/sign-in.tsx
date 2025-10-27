ef58ce3f120d6fa4463babae756200ab2fe03848
kamiye <kamiye31@gmail.com>
2025-10-27 19:15:09 +0100

    feat: remove styling from SignIn text component

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -5,7 +5,7 @@ import { router } from 'expo-router'
 const SignIn = () => {
   return (
     <View>
-      <Text className='text-red-500 text-5xl flex justify-center items-center'>SignIn</Text>
+      <Text className=''>SignIn</Text>
       <Button title='Sign In' onPress={() => router.push("/sign-up")} />
     </View>
   )
